5e3d4ceb13dc68e6b53237f0895d728bf742bd26
Derek <derex1987@gmail.com>
2026-01-24 05:50:30 -0600

    feat: merge sort progress

--- extract_derexXD_comments.py
+++ extract_derexXD_comments.py
@@ -12,8 +12,18 @@ flags = {
 # hard coded lol
 url = 'https://youtu.be/ytKVGtJ5yng?si=2bq3ltobiuo9lA4q'
 
-def merge(arr, left, mid, right):
+def merge(list, left, mid, right):
     """I wrote a merge sort, it is 5:32 AM and Texas Winter Storm apparently about to hit"""
+    # Merge sort takes O(n) space so it is not in place
+    # two subarrays that are going to be merged, taking a slice
+    L = list[left : mid + 1]
+    R = list[mid + 1 : right + 1]
+
+    i = 0 # Current position inside left subarray
+    j = 0 # Current position inside right subaway
+    k = left # Pos in original array
+
+    
 
 def mergeSort(list, left, right):
     """Split everything into tiny pieces. Then we begin sorting as the pieces are compared and put back together"""
