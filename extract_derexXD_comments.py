2acf27c3da8e97896e77e93e30b2fc4f7bfba3d2
Derek <derex1987@gmail.com>
2026-01-24 17:50:09 -0600

    chore: format python

--- extract_derexXD_comments.py
+++ extract_derexXD_comments.py
@@ -6,11 +6,12 @@ flags = {
     "skip_download": True,
     "writesubtitles": False,
     "writeautomaticsub": False,
-    "getcomments": True,  
-    }
+    "getcomments": True,
+}
 
 # hard coded lol
-url = 'https://youtu.be/ytKVGtJ5yng?si=2bq3ltobiuo9lA4q'
+url = "https://youtu.be/ytKVGtJ5yng?si=2bq3ltobiuo9lA4q"
+
 
 def merge(comments_list, left, mid, right):
     """I wrote a merge sort, it is 5:32 AM and Texas Winter Storm apparently about to hit"""
@@ -19,9 +20,9 @@ def merge(comments_list, left, mid, right):
     L = comments_list[left : mid + 1]
     R = comments_list[mid + 1 : right + 1]
 
-    i = 0 # Current position inside left subarray
-    j = 0 # Current position inside right subaway
-    k = left # Pos in original array
+    i = 0  # Current position inside left subarray
+    j = 0  # Current position inside right subaway
+    k = left  # Pos in original array
 
     # Over here we do the sorting comparison, and using index k (current position in built list) we add elements back, sorted
     while i < len(L) and j < len(R):
@@ -32,7 +33,7 @@ def merge(comments_list, left, mid, right):
             comments_list[k] = R[j]
             j += 1
         k += 1
-    
+
     # I just took this from geeksforgeeks i got tired
     # Copy the remaining elements of L[],
     # if there are any
@@ -41,14 +42,13 @@ def merge(comments_list, left, mid, right):
         i += 1
         k += 1
 
-    # Copy the remaining elements of R[], 
+    # Copy the remaining elements of R[],
     # if there are any
     while j < len(R):
         comments_list[k] = R[j]
         j += 1
         k += 1
 
-    
 
 def mergeSort(list, left, right):
     """Split everything into tiny pieces. Then we begin sorting as the pieces are compared and put back together"""
@@ -57,7 +57,6 @@ def mergeSort(list, left, right):
         mergeSort(list, left, mid)
         mergeSort(list, mid + 1, right)
         merge(list, left, mid, right)
-
 
 
 with YoutubeDL(flags) as derexXD:
