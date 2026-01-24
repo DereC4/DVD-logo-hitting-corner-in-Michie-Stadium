c2084c7148cf59856c72bb55c14c1a121421017b
Derek <derex1987@gmail.com>
2026-01-24 05:41:15 -0600

    feat: 5 AM leetcode when 2026 Texas storm about to hit

--- extract_derexXD_comments.py
+++ extract_derexXD_comments.py
@@ -10,8 +10,20 @@ flags = {
     }
 
 # hard coded lol
-
 url = 'https://youtu.be/ytKVGtJ5yng?si=2bq3ltobiuo9lA4q'
+
+def merge(arr, left, mid, right):
+    """I wrote a merge sort, it is 5:32 AM and Texas Winter Storm apparently about to hit"""
+
+def mergeSort(list, left, right):
+    """Split everything into tiny pieces. Then we begin sorting as the pieces are compared and put back together"""
+    if left < right:
+        mid = (left + right) // 2
+        mergeSort(list, left, mid)
+        mergeSort(list, mid + 1, right)
+        merge(list, left, mid, right)
+
+
 
 with YoutubeDL(flags) as derexXD:
     info = derexXD.extract_info(url, download=False)
