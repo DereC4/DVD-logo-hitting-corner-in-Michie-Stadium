0bf36d3b46c38ee9ffa2bb476b080593c4dc2718
Derek <derex1987@gmail.com>
2026-01-24 05:12:12 -0600

    feat: rewrite using package

--- extract_derexXD_comments.py
+++ extract_derexXD_comments.py
@@ -1,17 +1,30 @@
 import subprocess
+import json
+from yt_dlp import YoutubeDL
+
+flags = {
+    "skip_download": True,
+    "writesubtitles": False,
+    "writeautomaticsub": False,
+    "getcomments": True,  
+    }
 
 # hard coded lol
 
 url = 'https://youtu.be/ytKVGtJ5yng?si=2bq3ltobiuo9lA4q'
-subprocess.run(
-    [
-        "yt-dlp",
-        "--skip-download",
-        "--write-comments",
-        "--no-write-info-json",
-        "--output",
-        "comments",
-        url,
-    ],
-    check=True
-)
+
+with YoutubeDL(flags) as derexXD:
+    info = derexXD.extract_info(url, download=False)
+
+# subprocess.run(
+#     [
+#         "yt-dlp",
+#         "--skip-download",
+#         "--write-comments",
+#         "--no-write-info-json",
+#         "--output",
+#         "comments",
+#         url,
+#     ],
+#     check=True
+# )
