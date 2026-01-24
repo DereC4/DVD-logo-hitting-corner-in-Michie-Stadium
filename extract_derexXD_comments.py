ef3f5e67b5bab64285c0ded8396ef211cfda24c5
Derek <derex1987@gmail.com>
2026-01-24 06:03:52 -0600

    whew

--- extract_derexXD_comments.py
+++ extract_derexXD_comments.py
@@ -12,16 +12,41 @@ flags = {
 # hard coded lol
 url = 'https://youtu.be/ytKVGtJ5yng?si=2bq3ltobiuo9lA4q'
 
-def merge(list, left, mid, right):
+def merge(comments_list, left, mid, right):
     """I wrote a merge sort, it is 5:32 AM and Texas Winter Storm apparently about to hit"""
     # Merge sort takes O(n) space so it is not in place
     # two subarrays that are going to be merged, taking a slice
-    L = list[left : mid + 1]
-    R = list[mid + 1 : right + 1]
+    L = comments_list[left : mid + 1]
+    R = comments_list[mid + 1 : right + 1]
 
     i = 0 # Current position inside left subarray
     j = 0 # Current position inside right subaway
     k = left # Pos in original array
+
+    # Over here we do the sorting comparison, and using index k (current position in built list) we add elements back, sorted
+    while i < len(L) and j < len(R):
+        if L[i].get("like_count") >= R[j].get("like_count"):
+            comments_list[k] = L[i]
+            i += 1
+        else:
+            comments_list[k] = R[j]
+            j += 1
+        k += 1
+    
+    # I just took this from geeksforgeeks i got tired
+    # Copy the remaining elements of L[],
+    # if there are any
+    while i < len(L):
+        comments_list[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[], 
+    # if there are any
+    while j < len(R):
+        comments_list[k] = R[j]
+        j += 1
+        k += 1
 
     
 
